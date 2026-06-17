052819f844ebfc457878ef95061b75a0da8321f1
pedro bueno <pedrobueno@MacBook-Pro-de-pedro.local>
2026-06-17 20:28:01 -0300

    teste

--- pkg/Redis.go
+++ pkg/Redis.go
@@ -1,42 +1,10 @@
 package pkg
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/gomodule/redigo/redis"
-)
-
 // type ClientTenant struct {
 // 	app    string
 // 	slot   string
 // 	tenant string
 // }
-
-func RedisConn(address string, password string, database int) (redis.Conn, error) {
-	conn, err := redis.Dial(
-		"tcp",
-		address,
-		redis.DialPassword(password),
-		redis.DialDatabase(database),
-	)
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "[REDIS ERROR] => ", err)
-		return nil, err
-	}
-
-	data, err := conn.Do("PING")
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "[REDIS ERROR] => ", err)
-		return nil, err
-	}
-
-	fmt.Fprintln(os.Stdout, "[REDIS CONNECTION] => ", data)
-
-	return conn, nil
-}
 
 // func NewRedisConnection(address string, password string, database int) (*redis.Client, error) {
 // 	context := context.Background()
