10bb720c9c66a2832c4129e4f51c40bda7314a32
pedro bueno <pedrobueno@MacBook-Pro-de-pedro.local>
2026-06-22 10:07:22 -0300

    Ajusta RUnlock para liberação do cache

--- pkg/redis/cache/LocalCache.go
+++ pkg/redis/cache/LocalCache.go
@@ -51,7 +51,6 @@ func (lc *LocalCache) SetTenant(tenant *models.TenantSlot, ttl int) {
 func (lc *LocalCache) GetTenant(Id string) (*models.TenantSlot, error) {
 	lc.mu.RLock()
 	entry, exists := lc.cache[Id]
-	lc.mu.RUnlock()
 
 	if !exists {
 		lc.mu.RUnlock()
@@ -60,6 +59,7 @@ func (lc *LocalCache) GetTenant(Id string) (*models.TenantSlot, error) {
 	}
 
 	if time.Now().After(entry.expiresAt) {
+		lc.mu.RUnlock()
 		lc.mu.Lock()
 
 		// Garante que não teve modificações concorrentes entre uma escrita e outra
