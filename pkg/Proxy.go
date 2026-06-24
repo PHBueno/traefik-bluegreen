4c273273da941a79d1b970c34eb000f5cdc69bdd
pedro bueno <pedrobueno@MacBook-Pro-de-pedro.local>
2026-06-24 10:59:10 -0300

    [DEBUG] - path return test

--- pkg/Proxy.go
+++ pkg/Proxy.go
@@ -29,6 +29,8 @@ func (p *Proxy) RewriteProxy() func(*httputil.ProxyRequest) {
 		tenant := verifyEmpty(pr.In.URL.Query().Get("tenant"), "000000") // tenant default => 000000
 		app := verifyEmpty(pr.In.Header.Get("X-App-Slug"), "default")    // app default => default
 
+		slog.Info("Application Path", "path", pr.In.URL.Path)
+
 		tenantModel, err := p.RedisConn.GetSlot(tenant, app)
 		slot := "-1" // Caso não encontre o valor no Redis nem no Cache
 
